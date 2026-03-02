3e39a65858f98179ba50f3cf7e4d9c779fc15ca8
gDugonski <guidugonski1@gmail.com>
2026-03-02 16:07:57 -0300

    Estrutura da página de Login

--- website/src/Pages/Login.tsx
+++ website/src/Pages/Login.tsx
@@ -3,7 +3,13 @@ import '../Styles/Login.css'
 function Login(){
     return(
         <div>
-            <h1>Login Page</h1>
+            <h1>Login</h1>
+
+            <input type="text" placeholder='Digite seu email'/>
+            <input type="password" placeholder='Digite sua senha'/>
+            
+            <button>Entrar</button>
+
         </div>
     )
 }
